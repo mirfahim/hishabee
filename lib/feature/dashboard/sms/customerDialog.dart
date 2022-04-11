57b1cdfd7ef81d30a8fd64a219f2bc25081e2a6e
Samin Yeaser <saminyeaser1@gmail.com>
2022-04-11 23:50:23 +0600

    customer, supplier, employee contacts added in sms marketing

--- lib/feature/dashboard/sms/customerDialog.dart
+++ lib/feature/dashboard/sms/customerDialog.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:hishabee_business_manager_fl/app/modules/shop_main/data/remote/models/get_all_shop_response_model.dart';
+import 'package:hishabee_business_manager_fl/app/modules/single_shop/contacts/_binding/contact_binding.dart';
+import 'package:hishabee_business_manager_fl/app/modules/single_shop/contacts/_navigation/contact_pages.dart';
+import 'package:hishabee_business_manager_fl/app/modules/single_shop/contacts/_navigation/contact_routes.dart';
+import 'package:hishabee_business_manager_fl/app/modules/single_shop/contacts/data/remote/models/contact_type_model.dart';
 import 'package:hishabee_business_manager_fl/app/modules/single_shop/contacts/data/remote/models/customer_model.dart';
+import 'package:hishabee_business_manager_fl/app/modules/single_shop/contacts/presentation/pages/add_contact_page.dart';
 import 'package:hishabee_business_manager_fl/controllers/sms/sms_controller.dart';
+import 'package:hishabee_business_manager_fl/feature/dashboard/sms/addCustomer.dart';
 import 'package:hishabee_business_manager_fl/models/sms/sms_package_model.dart';
 
 class SmsCustomCustomerDialogContacts extends StatefulWidget {
@@ -67,32 +74,35 @@ class _SmsCustomCustomerDialogContactsState
                       borderRadius: BorderRadius.circular(10)),
                 ),
               ),
-              Container(
-                height: 300,
-                child: ListView.builder(
-                  itemCount: _foundData.length,
-                  shrinkWrap: true,
-                  scrollDirection: Axis.vertical,
-                  itemBuilder: (BuildContext context, index) => Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: GestureDetector(
-                      onTap: () {
-                        _smsController.selectedMobileNumber
-                            .add('\"${_foundData[index].mobile}\"');
-                        print(_smsController.selectedMobileNumber.length);
-                        print(_smsController.selectedMobileNumber);
-                      },
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                              '${_foundData[index].name}(${_foundData[index].mobile})'),
-                          Divider(
-                            thickness: 2,
-                            color: Colors.grey.withOpacity(.35),
-                          )
-                        ],
+              Expanded(
+                flex: 2,
+                child: Container(
+                  // height: 300,
+                  child: ListView.builder(
+                    itemCount: _foundData.length,
+                    shrinkWrap: true,
+                    scrollDirection: Axis.vertical,
+                    itemBuilder: (BuildContext context, index) => Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          _smsController.selectedMobileNumber
+                              .add('\"${_foundData[index].mobile}\"');
+                          print(_smsController.selectedMobileNumber.length);
+                          print(_smsController.selectedMobileNumber);
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                                '${_foundData[index].name}(${_foundData[index].mobile})'),
+                            Divider(
+                              thickness: 2,
+                              color: Colors.grey.withOpacity(.35),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
@@ -118,7 +128,17 @@ class _SmsCustomCustomerDialogContactsState
                         ),
                       ),
                       ElevatedButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          Get.to(CustomerAdd(shopId: '${storageSms.read("shop_id")}',));
+                          // Customer customer = new Customer();
+                          // Get.toNamed(ContactRoutes.ADD_CONTACT,
+                          //     arguments: {
+                          //       'shop': Shop,
+                          //       "type": ContactType.CUSTOMER,
+                          //       "contact": customer,
+                          //     }
+                          // );
+                        },
                         child: Text('Add Customer'),
                       ),
                     ],
