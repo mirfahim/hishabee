2becb1bb07abc86d3fab7bf94c3cced2c5d424e5
MirFahim <mirfahim@aapbd.com>
2022-05-12 13:25:32 +0600

    new

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/jahangir/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/jahangir/Hishabee_Flutter"
+export "FLUTTER_ROOT=/Users/aapbdoffice/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/aapbdoffice/Desktop/flutterprojects/HishabeeBusinessFlutter/HishabeeBusinessFlutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "EXCLUDED_ARCHS=arm64"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.packages"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
